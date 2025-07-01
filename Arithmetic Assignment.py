f282f80d28f4f5e5a5b7ca7b1f905e99168b330b
Zunair_Riaz <ranazunairriaz@gmail.com>
2025-07-02 02:11:13 +0500

    Add files via upload

---
+++ Arithmetic Assignment.py
@@ -0,0 +1,44 @@
+#Use Shift + Alt to duplicate the line
+
+#Addition
+x=7
+y=5
+result = x+y
+print(f"{x} + {y} = {result}")
+
+#Substraction 
+x=7
+y=5
+result = x-y
+print(f"{x} - {y} = {result}")
+
+#Multiplication
+x=7
+y=5
+result = x*y
+print(f"{x} * {y} ={result}")
+
+#Division
+x=7
+y=5
+result = x/y
+print(f"{x} / {y} = {result}")
+
+#Floor Division
+x=7
+y=5
+result = x // y
+print(f"{x} // {y} ={result}")
+
+#Modulus
+x=7
+y=5
+result = x%y
+print(f"{x} % {y} = {result}")
+
+#Exponent
+
+x=7
+y=5
+result = x**y
+print(f"{x} ** {y} = {result}")
